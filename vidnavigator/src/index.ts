29c88b1fcd854682baa639e8f30762773c16a8e9
hatem.mezlini <hatem.mezlini@evina.com>
2026-05-15 16:51:30 +0200

    fix

--- vidnavigator/src/index.ts
+++ vidnavigator/src/index.ts
@@ -59,7 +59,7 @@ export * from './models';
 export * from './errors';
 
 /** SDK version (keep in sync with package.json) */
-export const SDK_VERSION = '1.0.3';
+export const SDK_VERSION = '1.0.4';
 
 //region --- Interfaces ---
 export interface SDKConfig {
